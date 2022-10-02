dcf615bdb9c711e37c27e945b2ef51790cae70f2
katapy <katapy.tennis@gmail.com>
2022-10-02 21:16:22 +0900

    github action test2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
 // module.exports = nextConfig
+
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [withStaticImport, withBundleAnalyzer, withCustomWebpack]
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...defaultConfig, ...config })
+}
